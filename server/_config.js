be418585cc146a284fbaf00d68e4f0f4226e00b2
Arvind Tomar <tomararvindsingh09@gmail.com>
2016-08-08 15:34:41 +0530

    configuration changes

--- server/_config.js
+++ server/_config.js
@@ -1,54 +1,54 @@
 var ids = {
-  github: {
-    clientID: '7719rof6v75s4g',
-    clientSecret: 'fM4ACEGDCVaYdNfg',
-    callbackURL: "https://192.168.200.67:8000/auth/github/callback"
-  },
-  linkedin: {
-    clientID: '7719rof6v75s4g',
-    clientSecret: 'fM4ACEGDCVaYdNfg',
-    callbackURL: "https://192.168.200.67:8000/auth/linkedin/callback"
-  },
-  twitter: {
-    consumerKey: 'iObC2FyT9385PweLYRXMoGvrF',
-    consumerSecret: 'ROgNGXq1k6fAY9sWFI0hyEGYHHiwbzEk11Lkjs3Pc7LZFID8Cp',
-    callbackURL: "https://192.168.200.67:8000/auth/twitter/callback"
-  },
-  facebook: {
-    clientID: '1082456341840678',
-    clientSecret: '1790a63ec214dc086feec3be2a2e0b82',
-    callbackURL: "https://192.168.200.67:8000/auth/facebook/callback"
-  },
-  tumblr: {
-    consumerKey: 'VlgkvmTCPPA1iwvoi5Ey5Py5y4odGruJV6SH17SjuPBcvNYtvS',
-    consumerSecret: 'BS9AsDtIdp8ZfSrxy5nxxOMpBIcExqTK1xdZH5xAu4FCuqbQKW',
-    callbackURL: "https://192.168.200.67:8000/auth/tumblr/callback"
-  },
-  yahoo: {
-    consumerKey: 'VlgkvmTCPPA1iwvoi5Ey5Py5y4odGruJV6SH17SjuPBcvNYtvS',
-    consumerSecret: 'BS9AsDtIdp8ZfSrxy5nxxOMpBIcExqTK1xdZH5xAu4FCuqbQKW',
-    returnURL: "https://192.168.200.67:8000/auth/yahoo/callback"
-  },
-  google: {
-    consumerKey:'528871124361-n08g7ap1sda52rjbegrg6qh2nu23es0c.apps.googleusercontent.com',
-    consumerSecret:'s0rk7lfP2vDG1-mmHf2oq1GU',
-     returnURL: "https://192.168.200.67:8000/auth/google/callback"
-  },
-  windowslive:{
-    clientID:'528871124361-n08g7ap1sda52rjbegrg6qh2nu23es0c.apps.googleusercontent.com',
-    clientSecret:'s0rk7lfP2vDG1-mmHf2oq1GU',
-    callbackURL: "https://192.168.200.67:8000/auth/windowslive/callback"
-  },
-  dropbox: {
-    consumerKey: 'ZUghSAL4QOPXCYpk5Y1SZjnKH',
-    consumerSecret: 'tBGcbOwQOyl4vOcUSiSIK1NKXRoerrnCp5Jhs77rUB23w73ZDe',
-    callbackURL: "https://192.168.200.67:8000/auth/dropbox/callback"
-  },
-  serverURI: "gluu.local.org",
-  serverWebPort: 8000,
-  applicationEndpoint: "https://gluu.local.org/oxauth/auth/generic/genericpostlogin.htm",
-  applicationStartpoint: "https://gluu.local.org/oxauth/auth/generic/genericlogin.htm",
-  applicationSecretKey: "GluuNodeServerSocialLogin1234567890"
+    github: {
+        clientID: 'key',
+        clientSecret: 'secret',
+        callbackURL: "callbackURL"
+    },
+    linkedin: {
+        clientID: 'key',
+        clientSecret: 'secret',
+        callbackURL: "callbackURL"
+    },
+    twitter: {
+        consumerKey: 'key',
+        consumerSecret: 'secret',
+        callbackURL: "callbackURL"
+    },
+    facebook: {
+        clientID: 'key',
+        clientSecret: 'secret',
+        callbackURL: "callbackURL"
+    },
+    tumblr: {
+        consumerKey: 'key',
+        consumerSecret: 'secret',
+        callbackURL: "callbackURL"
+    },
+    yahoo: {
+        consumerKey: 'key',
+        consumerSecret: 'secret',
+        returnURL: "callbackURL"
+    },
+    google: {
+        consumerKey: 'key',
+        consumerSecret: 'secret',
+        returnURL: "callbackURL"
+    },
+    windowslive: {
+        clientID: 'key',
+        clientSecret: 'secret',
+        callbackURL: "callbackURL"
+    },
+    dropbox: {
+        consumerKey: 'key',
+        consumerSecret: 'secret',
+        callbackURL: "callbackURL"
+    },
+    serverURI: "hostname",
+    serverWebPort: 8000,
+    applicationEndpoint: "applicationEndpoint",
+    applicationStartpoint: "applicationStartpoint",
+    applicationSecretKey: "applicationSecretKey"
 };
 
 module.exports = ids;
