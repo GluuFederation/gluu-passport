e660a60d7ee25b627e47e3ef747e96e7446878ac
Jose G <bonustrack310@gmail.com>
2018-05-29 10:13:45 -0500

    Fixes for #32, #33

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -2,6 +2,7 @@ var fs = require('fs'),
     Stomp = require('stomp-client'),
     winston = require('winston'),
     dir = process.env.NODE_LOGGING_DIR || __dirname + '/logs';
+
 require('winston-daily-rotate-file');
 var dateFormat = require('dateformat');
 
@@ -12,13 +13,13 @@ if (!fs.existsSync(dir)){
 winston.emitErrs = true;
 
 var logOpts = {
-    level: 'info',
+    level: global.config.logLevel ? global.config.logLevel : 'info',
     filename: dir + '/passport-%DATE%.log',
     handleExceptions: true,
     json: false,
     colorize: false,
     tailable: true,
-    datePattern: 'YYYY-MM-DD-HH',
+    datePattern: 'YYYY-MM-DD',
     zippedArchive: true,
     maxSize: '20m',
     maxFiles: '14d',
