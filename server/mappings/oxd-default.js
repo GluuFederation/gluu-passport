dbe9fdb2c0f2a342d4535392c01ab12c104b685c
Jose G <bonustrack310@gmail.com>
2019-04-16 14:18:28 -0500

    Adjust default oxd mapping

--- server/mappings/oxd-default.js
+++ server/mappings/oxd-default.js
@@ -1,11 +1,10 @@
 module.exports = profile => {
-	let claims = profile.claims
 	return {
-		uid: claims.user_name,
-		mail: claims.email,
-		cn: claims.name,
-		displayName: claims.name,
-		givenName: claims.given_name,
-		sn: claims.family_name
+		uid: profile.user_name || profile.sub,
+		mail: profile.email,
+		cn: profile.name,
+		displayName: profile.name,
+		givenName: profile.given_name,
+		sn: profile.family_name
 	}
 }
