f38edbc12cb105f24a16f8bd773f6a0957d79a68
Christian H <eland.christian@gmail.com>
2020-07-08 15:07:55 +0000

    organizing mocks

--- test/idp-initiated.test.js
+++ test/idp-initiated.test.js
@@ -31,138 +31,22 @@ const helper = require('./helper.js')
 */
 
 // consts to mock
+
 const jwt = "eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCIsImtpZCI6IjM2NjU4ZTAzLTM0ZWEtNDc0NS1hZDQzLTk1OTkxNmM5NmRlZl9zaWdfcnM1MTIifQ.eyJpc3MiOiJodHRwczovL2NocmlzLmdsdXV0aHJlZS5vcmciLCJzdWIiOlsidGVzdGVyMyJdLCJhdWQiOiIxNTAzLmYxOTE3ZjZmLWIxNTUtNDJlMC05YmQxLTk5ZDU2ZjVjM2I1MCIsImp0aSI6IjMxYTFmNGUwLTE5YzUtNDMwMi1hYzE4LWY1OTk2YjcxYzVlNyIsImV4cCI6MTU5NDE4MDc2NC45OSwiaWF0IjoxNTk0MTgwNzM0OTkwLCJkYXRhIjoiQmVHMG1BU2lWUys2K3RUSDhUWFZNZ0lpOW9zTXFXTG1WVi9sMC9GSTdEWDBBQzRXQ3hlK1hhQWpiNnNPZWVBQksyUEIyUGVhZ0FSM3lQcExBSE52YjYvOUVpK1JjY3RlZ3haYUhEYlpRdEsydGFzN3JPZlFFUmNkRDNKSFFXaWlneFphSERiWlF0Snpycml6WXV0TXNyUG9vVklpMG9ZajlTYWM3Sk16a0NqWVNQM2dSU3phMXJaQUt5Mmw4VEVRejRxYVQxWjFzSHQ3TEpzem9OZTcvcmNLZ0pVM0wwbW1qbCtYNDkwRmJEVT0ifQ.G7O9uIP00vtsSsmjs8IPf9Tl-6Q9YE-gh-t5_-K2HKoO7akSLUJegvu4qYjaC9Din1fkRKJZpndGUiiJfcpACum4llw1GIC_UqPkDZhO2i5azwqy_nBRLHOxdyd_DchP2OjVYUMMwzZIzh2nUNQYk7NOqMKV05nd-YausfG6cFECqEhu_J0glUsm41cs066shv9UfU2fwNyPXSlOSVupYW6Ey6KjP03t5E44m0Ab09N0pHqIJlFBxYi4xb64RXzDhfjfV2SsWZl8XHAuY5mdrdCFxT6m6fZuEMAS6k9izCuq99-sPLrRBXshAjbpdoNzIBg60TOMgxyI8gsI9b38HA"
 
-const basicConfig = {
-	'configurationEndpoint':
-	'https://chris.gluuthree.org/identity/restv1/passport/config',
-	'failureRedirectUrl':
-	'https://chris.gluuthree.org/oxauth/auth/passport/passportlogin.htm',
-	'logLevel': 'debug',
-	'consoleLogOnly': false,
-	'clientId': '1502.d49baf9f-b19b-40de-a990-33d08e7f9e77',
-	'keyPath': '/etc/certs/passport-rp.pem',
-	'keyId': '36658e03-34ea-4745-ad43-959916c96def_sig_rs512',
-	'keyAlg': 'RS512'
-}
+const basicConfig = config.get('passportFile')
 
-const conf = {
-	'serverURI' : 'https://chris.gluuthree.org',
-	'serverWebPort' : 8090,
-	'postProfileEndpoint' : 'https://chris.gluuthree.org/oxauth/postlogin.htm',
-	'spTLSCert' : '/etc/certs/passport-sp.crt',
-	'spTLSKey' : '/etc/certs/passport-sp.key',
-	'logging' : {
-		'level' : 'debug',
-		'consoleLogOnly' : false,
-		'activeMQConf' : {
-			'enabled' : false,
-			'host' : '',
-			'username' : '',
-			'password' : '',
-			'port' : 0
-		}
-	}
-}
-const scope = nock('https://chris.gluuthree.org')
-	.get('/identity/restv1/passport/config')
-	.reply( 200, {
-		'conf' : conf,
-		'idpInitiated' : {
-			'openidclient' : {
-				'authorizationEndpoint' : 'https://chris.gluuthree.org/oxauth/restv1/authorize',
-				'clientId' : '1503.f1917f6f-b155-42e0-9bd1-99d56f5c3b50',
-				'acrValues' : 'passport_saml'
-			},
-			'authorizationParams' : [ {
-				'provider' : 'saml-yidpinitiated',
-				'extraParams' : { },
-				'redirect_uri' : 'https://chris.gluuthree.org/oxauth/auth/passport/sample-redirector.htm',
-				'response_type' : 'code',
-				'scope' : 'openid'
-			} ]
-		},
-		'providers' : [ {
-			'id' : 'saml-only-1',
-			'displayName' : 'saml only 1',
-			'type' : 'saml',
-			'mapping' : 'saml_ldap_profile',
-			'passportStrategyId' : 'passport-saml',
-			'enabled' : true,
-			'callbackUrl' : 'https://chris.gluuthree.org/passport/auth/saml/saml-only-1/callback',
-			'requestForEmail' : false,
-			'emailLinkingSafe' : false,
-			'options' : {
-				'skipRequestCompression' : 'True',
-				'authnRequestBinding' : 'HTTP-POST',
-				'identifierFormat' : 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
-				'cert' : 'MIIDlzCCAn8CFBgf85Th/k9LW/WX1Tm2K8L46XFKMA0GCSqGSIb3DQEBCwUAMIGHMQswCQYDVQQGEwJCUjELMAkGA1UECAwCU1AxEjAQBgNVBAcMCVNhbyBQYXVsbzEZMBcGA1UECgwQQ2hyaXMgVGVzdGluZyBuQzEaMBgGA1UEAwwRY2hyaXMuZ2x1dXR3by5vcmcxIDAeBgkqhkiG9w0BCQEWEWNocmlzQHRlc3RpbmcuY29tMB4XDTIwMDYyMzE0NDU1M1oXDTIxMDYyMzE0NDU1M1owgYcxCzAJBgNVBAYTAkJSMQswCQYDVQQIDAJTUDESMBAGA1UEBwwJU2FvIFBhdWxvMRkwFwYDVQQKDBBDaHJpcyBUZXN0aW5nIG5DMRowGAYDVQQDDBFjaHJpcy5nbHV1dHdvLm9yZzEgMB4GCSqGSIb3DQEJARYRY2hyaXNAdGVzdGluZy5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDIaxbLrWDti7ZLAU4YVxNR6bkjt/HDfczBNF5ULlqttTbP65HgOMAl9eI8Sg+vPN2y7lk7ogQW4bJ3gcBfiBjanU8jrVMntXB8VwhZ8YYThkg1NBb9KPf9sW6FsOz+LDKNxJQeXu7jbKtb7KZvAQiFWCLil6VuKgvmjcDSnRARkSSacqVs7vM/OH9t+zRdeLA2LFEfUIW1GoOi66Tmt6hnVIhIm9I6vJOE+ym0HnyqPUQy6ZEWGbVbJ4Fn9JJmoZ3jJ1v9ZxfKJt2ZCz2HydOWJHXyg2fZwCBVdoJcydtVWQFNVJMEvQUCZNofyiJsCu+rQ033NWyhtrjlYL2fEqRnAgMBAAEwDQYJKoZIhvcNAQELBQADggEBABDbtviA7rVkg/8wPRYPgi07jCoR9x7ZnJjMB4xHFgwIKRF7FKapUBOvqzSmYbNm3JotAdq6o9gPD3rEjQh4Sy2fptA64fquY6Fo5paVTL5AECdumv67+ziB5mtYE0iabY+QHcLHpy6kqJvFpaeUeBNypvx6SaZ3BM/9Q5VwEmmuuf+VAnY/7Q/BHVUhUBeNs9G1LOtqLTr56QyOO4ET1NKihAeE8A/R05O7fELlB2HJ4LxhMLfzwQwQIzAg5fxYrZLtjGu524SSL7Xb6BuLIitwZVAYBcXS2Up37NGHdQu9c2uHFQoxk+ZNKO1ZRUl7IE/8c6DjMTRXRpZqqRaUBco=',
-				'entryPoint' : 'https://chris.gluutwo.org/idp/profile/SAML2/POST/SSO',
-				'issuer' : 'urn:test:one'
-			}
-		}, {
-			'id' : 'saml-emailreq',
-			'displayName' : 'saml-emailreq',
-			'type' : 'saml',
-			'mapping' : 'saml_ldap_profile',
-			'passportStrategyId' : 'passport-saml',
-			'enabled' : true,
-			'callbackUrl' : 'https://chris.gluuthree.org/passport/auth/saml/saml-emailreq/callback',
-			'requestForEmail' : true,
-			'emailLinkingSafe' : false,
-			'options' : {
-				'skipRequestCompression' : 'True',
-				'authnRequestBinding' : 'HTTP-POST',
-				'identifierFormat' : 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
-				'cert' : 'MIIDlzCCAn8CFBgf85Th/k9LW/WX1Tm2K8L46XFKMA0GCSqGSIb3DQEBCwUAMIGHMQswCQYDVQQGEwJCUjELMAkGA1UECAwCU1AxEjAQBgNVBAcMCVNhbyBQYXVsbzEZMBcGA1UECgwQQ2hyaXMgVGVzdGluZyBuQzEaMBgGA1UEAwwRY2hyaXMuZ2x1dXR3by5vcmcxIDAeBgkqhkiG9w0BCQEWEWNocmlzQHRlc3RpbmcuY29tMB4XDTIwMDYyMzE0NDU1M1oXDTIxMDYyMzE0NDU1M1owgYcxCzAJBgNVBAYTAkJSMQswCQYDVQQIDAJTUDESMBAGA1UEBwwJU2FvIFBhdWxvMRkwFwYDVQQKDBBDaHJpcyBUZXN0aW5nIG5DMRowGAYDVQQDDBFjaHJpcy5nbHV1dHdvLm9yZzEgMB4GCSqGSIb3DQEJARYRY2hyaXNAdGVzdGluZy5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDIaxbLrWDti7ZLAU4YVxNR6bkjt/HDfczBNF5ULlqttTbP65HgOMAl9eI8Sg+vPN2y7lk7ogQW4bJ3gcBfiBjanU8jrVMntXB8VwhZ8YYThkg1NBb9KPf9sW6FsOz+LDKNxJQeXu7jbKtb7KZvAQiFWCLil6VuKgvmjcDSnRARkSSacqVs7vM/OH9t+zRdeLA2LFEfUIW1GoOi66Tmt6hnVIhIm9I6vJOE+ym0HnyqPUQy6ZEWGbVbJ4Fn9JJmoZ3jJ1v9ZxfKJt2ZCz2HydOWJHXyg2fZwCBVdoJcydtVWQFNVJMEvQUCZNofyiJsCu+rQ033NWyhtrjlYL2fEqRnAgMBAAEwDQYJKoZIhvcNAQELBQADggEBABDbtviA7rVkg/8wPRYPgi07jCoR9x7ZnJjMB4xHFgwIKRF7FKapUBOvqzSmYbNm3JotAdq6o9gPD3rEjQh4Sy2fptA64fquY6Fo5paVTL5AECdumv67+ziB5mtYE0iabY+QHcLHpy6kqJvFpaeUeBNypvx6SaZ3BM/9Q5VwEmmuuf+VAnY/7Q/BHVUhUBeNs9G1LOtqLTr56QyOO4ET1NKihAeE8A/R05O7fELlB2HJ4LxhMLfzwQwQIzAg5fxYrZLtjGu524SSL7Xb6BuLIitwZVAYBcXS2Up37NGHdQu9c2uHFQoxk+ZNKO1ZRUl7IE/8c6DjMTRXRpZqqRaUBco=',
-				'entryPoint' : 'https://chris.gluutwo.org/idp/profile/SAML2/POST/SSO',
-				'issuer' : 'urn:test:threemailreq'
-			}
-		}, {
-			'id' : 'saml-emaillink',
-			'displayName' : 'saml-emaillink',
-			'type' : 'saml',
-			'mapping' : 'saml_ldap_profile',
-			'passportStrategyId' : 'passport-saml',
-			'enabled' : true,
-			'callbackUrl' : 'https://chris.gluuthree.org/passport/auth/saml/saml-emaillink/callback',
-			'requestForEmail' : false,
-			'emailLinkingSafe' : true,
-			'options' : {
-				'skipRequestCompression' : 'True',
-				'authnRequestBinding' : 'HTTP-POST',
-				'identifierFormat' : 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
-				'cert' : 'MIIDlzCCAn8CFBgf85Th/k9LW/WX1Tm2K8L46XFKMA0GCSqGSIb3DQEBCwUAMIGHMQswCQYDVQQGEwJCUjELMAkGA1UECAwCU1AxEjAQBgNVBAcMCVNhbyBQYXVsbzEZMBcGA1UECgwQQ2hyaXMgVGVzdGluZyBuQzEaMBgGA1UEAwwRY2hyaXMuZ2x1dXR3by5vcmcxIDAeBgkqhkiG9w0BCQEWEWNocmlzQHRlc3RpbmcuY29tMB4XDTIwMDYyMzE0NDU1M1oXDTIxMDYyMzE0NDU1M1owgYcxCzAJBgNVBAYTAkJSMQswCQYDVQQIDAJTUDESMBAGA1UEBwwJU2FvIFBhdWxvMRkwFwYDVQQKDBBDaHJpcyBUZXN0aW5nIG5DMRowGAYDVQQDDBFjaHJpcy5nbHV1dHdvLm9yZzEgMB4GCSqGSIb3DQEJARYRY2hyaXNAdGVzdGluZy5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDIaxbLrWDti7ZLAU4YVxNR6bkjt/HDfczBNF5ULlqttTbP65HgOMAl9eI8Sg+vPN2y7lk7ogQW4bJ3gcBfiBjanU8jrVMntXB8VwhZ8YYThkg1NBb9KPf9sW6FsOz+LDKNxJQeXu7jbKtb7KZvAQiFWCLil6VuKgvmjcDSnRARkSSacqVs7vM/OH9t+zRdeLA2LFEfUIW1GoOi66Tmt6hnVIhIm9I6vJOE+ym0HnyqPUQy6ZEWGbVbJ4Fn9JJmoZ3jJ1v9ZxfKJt2ZCz2HydOWJHXyg2fZwCBVdoJcydtVWQFNVJMEvQUCZNofyiJsCu+rQ033NWyhtrjlYL2fEqRnAgMBAAEwDQYJKoZIhvcNAQELBQADggEBABDbtviA7rVkg/8wPRYPgi07jCoR9x7ZnJjMB4xHFgwIKRF7FKapUBOvqzSmYbNm3JotAdq6o9gPD3rEjQh4Sy2fptA64fquY6Fo5paVTL5AECdumv67+ziB5mtYE0iabY+QHcLHpy6kqJvFpaeUeBNypvx6SaZ3BM/9Q5VwEmmuuf+VAnY/7Q/BHVUhUBeNs9G1LOtqLTr56QyOO4ET1NKihAeE8A/R05O7fELlB2HJ4LxhMLfzwQwQIzAg5fxYrZLtjGu524SSL7Xb6BuLIitwZVAYBcXS2Up37NGHdQu9c2uHFQoxk+ZNKO1ZRUl7IE/8c6DjMTRXRpZqqRaUBco=',
-				'entryPoint' : 'https://chris.gluutwo.org/idp/profile/SAML2/POST/SSO',
-				'issuer' : 'https://chris.gluuthree.org/'
-			}
-		}, {
-			'id' : 'saml-yidpinitiated',
-			'displayName' : 'saml-yidpinitiated',
-			'type' : 'saml',
-			'mapping' : 'saml_ldap_profile',
-			'passportStrategyId' : 'passport-saml',
-			'enabled' : true,
-			'callbackUrl' : 'https://chris.gluuthree.org/passport/auth/saml/saml-yidpinitiated/callback',
-			'requestForEmail' : false,
-			'emailLinkingSafe' : false,
-			'options' : {
-				'skipRequestCompression' : 'true',
-				'authnRequestBinding' : 'HTTP-POST',
-				'validateInResponseTo' : 'false',
-				'identifierFormat' : 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
-				'cert' : 'MIIDlzCCAn8CFBgf85Th/k9LW/WX1Tm2K8L46XFKMA0GCSqGSIb3DQEBCwUAMIGHMQswCQYDVQQGEwJCUjELMAkGA1UECAwCU1AxEjAQBgNVBAcMCVNhbyBQYXVsbzEZMBcGA1UECgwQQ2hyaXMgVGVzdGluZyBuQzEaMBgGA1UEAwwRY2hyaXMuZ2x1dXR3by5vcmcxIDAeBgkqhkiG9w0BCQEWEWNocmlzQHRlc3RpbmcuY29tMB4XDTIwMDYyMzE0NDU1M1oXDTIxMDYyMzE0NDU1M1owgYcxCzAJBgNVBAYTAkJSMQswCQYDVQQIDAJTUDESMBAGA1UEBwwJU2FvIFBhdWxvMRkwFwYDVQQKDBBDaHJpcyBUZXN0aW5nIG5DMRowGAYDVQQDDBFjaHJpcy5nbHV1dHdvLm9yZzEgMB4GCSqGSIb3DQEJARYRY2hyaXNAdGVzdGluZy5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDIaxbLrWDti7ZLAU4YVxNR6bkjt/HDfczBNF5ULlqttTbP65HgOMAl9eI8Sg+vPN2y7lk7ogQW4bJ3gcBfiBjanU8jrVMntXB8VwhZ8YYThkg1NBb9KPf9sW6FsOz+LDKNxJQeXu7jbKtb7KZvAQiFWCLil6VuKgvmjcDSnRARkSSacqVs7vM/OH9t+zRdeLA2LFEfUIW1GoOi66Tmt6hnVIhIm9I6vJOE+ym0HnyqPUQy6ZEWGbVbJ4Fn9JJmoZ3jJ1v9ZxfKJt2ZCz2HydOWJHXyg2fZwCBVdoJcydtVWQFNVJMEvQUCZNofyiJsCu+rQ033NWyhtrjlYL2fEqRnAgMBAAEwDQYJKoZIhvcNAQELBQADggEBABDbtviA7rVkg/8wPRYPgi07jCoR9x7ZnJjMB4xHFgwIKRF7FKapUBOvqzSmYbNm3JotAdq6o9gPD3rEjQh4Sy2fptA64fquY6Fo5paVTL5AECdumv67+ziB5mtYE0iabY+QHcLHpy6kqJvFpaeUeBNypvx6SaZ3BM/9Q5VwEmmuuf+VAnY/7Q/BHVUhUBeNs9G1LOtqLTr56QyOO4ET1NKihAeE8A/R05O7fELlB2HJ4LxhMLfzwQwQIzAg5fxYrZLtjGu524SSL7Xb6BuLIitwZVAYBcXS2Up37NGHdQu9c2uHFQoxk+ZNKO1ZRUl7IE/8c6DjMTRXRpZqqRaUBco=',
-				'entryPoint' : 'https://chris.gluutwo.org/idp/profile/SAML2/POST/SSO',
-				'issuer' : 'chris.testingenv.org'
-			}
-		} ]
-	})
+const mocked_conf = helper.getMockedConfiguration()['conf']
+
+const scope = helper.nockMockedConfigurationRequest()
+
 
 
 
 describe('idp-initiated.createAuthzRequest', () => {
 
 	const valid_provider = 'saml-yidpinitiated'
+
 	const valid_user = {
 		cn : 'tester3',
 		displayName : 'tester3',
@@ -171,7 +55,9 @@ describe('idp-initiated.createAuthzRequest', () => {
 		sn : 'Testerlast2',
 		uid :'tester3'
 	}
+
 	const valid_extraParams = {}
+	
 	const valid_iiconfig = {
 		authorizationParams: [{
 			provider : 'saml-yidpinitiated',
@@ -189,21 +75,20 @@ describe('idp-initiated.createAuthzRequest', () => {
 	}
 
 
-
-	// with dots
-	const jwt_example = 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCIsImtpZCI6IjM2NjU4ZTAzLTM0ZWEtNDc0NS1hZDQzLTk1OTkxNmM5NmRlZl9zaWdfcnM1MTIifQ.eyJpc3MiOiJodHRwczovL2NocmlzLmdsdXV0aHJlZS5vcmciLCJzdWIiOlsidGVzdGVyNCJdLCJhdWQiOiIxNTAzLmYxOTE3ZjZmLWIxNTUtNDJlMC05YmQxLTk5ZDU2ZjVjM2I1MCIsImp0aSI6IjBmZGU5OGRjLWM3MWYtNDQ3ZS1iYWJlLTAwMWRlZDY1OTgyZSIsImV4cCI6MTU5NDE0NzkxMS4wOTgsImlhdCI6MTU5NDE0Nzg4MTA5OCwiZGF0YSI6IkJlRzBtQVNpVlM4SmRVdDJUYlQzWVFJaTlvc01xV0xtVlYvbDAvRkk3RFVobTB6OG9JRUhHS0FqYjZzT2VlQUJLMlBCMlBlYWdBU0VWREFSQysyc0ZhLzlFaStSY2N0ZWd4WmFIRGJaUXRLVEU4MGU1OHVST3hjZEQzSkhRV2lpZ3haYUhEYlpRdExqaFBPaHV1dmlXbWg0M1JOemF2cnY0RGh1dWc2bi9PbTdzeGZxaThlblpYYkpWdnNLblJaTVU2V05HT2FWRXpLOHowY3ZTY1VIMi9oUjh3L0dSSGhyIn0.m1CIbFfV1JNiFgb8hARoBAocc-a-Gsm6tn9xyGTX6ZGNPs1lu-Zdek_uKbb8t-BkYAInyAF355L4DR9Y77y8KfS22AeLjUqPvAOxyT5dziBhUAdacgSETiOrzRF6zuYsNJCI9Uok4v0kISLetXku9ZqiI1R57y1F1a4bOstB90EC_7I3xqrDSM0-BIUQlPZh66KcJY6Lw3Zyh-r41gP0VXghrO4DSWRMrWXmQuXIH_lQq-iaE-OnJMdW-ZeYEyFHSDdLl4VAV2YYUtokOZSIGI8Jg5xlet-re-CCZ7K9Ajedlq_OhgNTa1UfDbK2pHKePpH9qvzQjgOyTUhi699CTA'
-
-
 	// "importing" not exported function
 	var createAuthzRequest = idp_initiated.__get__('createAuthzRequest')
 
 	beforeEach(() => {
-		idp_initiated.__set__('config', conf)
+		idp_initiated.__set__('config', mocked_conf)
 		idp_initiated.__set__('basicConfig', basicConfig)
 		idp_initiated.__set__('jwt', jwt)
+		nock.disableNetConnect()
 		// idp_initiated.__set__('logger', logger)
 	})
 
+	afterEach(() => {
+		nock.enableNetConnect()
+	})
 	it('createAuthzRequest should exist', () => {
 		assert.exists(
 			createAuthzRequest,
