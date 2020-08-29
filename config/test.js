0773dac70a33c0e6c3d632f8c1ba4b2c6eecd680
christian-hawk <eland.christian@gmail.com>
2020-08-29 01:20:23 +0000

    brought passport file mocking to test config file

--- config/test.js
+++ config/test.js
@@ -1,9 +1,29 @@
 const saltFile = './test/testdata/salt'
-const passportFile = '/tmp/passport_config.json';
-const timerInterval = 60000;
+const timerInterval = 60000
+
+const passportFile = {
+	configurationEndpoint:
+        'https://chris.gluuthree.org/identity/restv1/passport/config',
+
+	failureRedirectUrl:
+        'https://chris.gluuthree.org/oxauth/auth/passport/passportlogin.htm',
+
+	logLevel: 'debug',
+
+	consoleLogOnly: false,
+
+	clientId: '1502.d49baf9f-b19b-40de-a990-33d08e7f9e77',
+
+	keyPath: './test/testdata/passport-rp.pem',
+
+	keyId: '36658e03-34ea-4745-ad43-959916c96def_sig_rs512',
+
+	keyAlg: 'RS512'
+}
+
 
 module.exports = {
-    saltFile,
-    passportFile,
-    timerInterval,
+	saltFile,
+	passportFile,
+	timerInterval,
 }
