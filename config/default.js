740f6ffe41b4c52f01933ae2c2f37b2ee7ff08c2
christian-hawk <eland.christian@gmail.com>
2020-08-29 01:13:47 +0000

    added config file json creation to dev env

--- config/default.js
+++ config/default.js
@@ -1,6 +1,41 @@
+const fs = require('fs')
+
+const passportConfigFileData = {
+	configurationEndpoint: "https://chris.gluuthree.org/identity/restv1/passport/config",
+	failureRedirectUrl: "https://chris.gluuthree.org/oxauth/auth/passport/passportlogin.htm",
+	logLevel: "debug",
+	consoleLogOnly: false,
+	clientId: "1502.3fe76d0a-38dd-4f91-830b-e33fd70d778a",
+	keyPath: "/tmp/passport-rp.pem",
+	keyId: "fbc267ef-0705-4b3a-8c80-bf70e75cf08b_sig_rs512",
+	keyAlg: "RS512"
+}
+
+/**
+ * Gets the default passport configuration file path.
+ * If file doesn't exists, creates it and return path
+ * @returns {string} passport-config.json file path
+ */
+function passportConfigFilePath() {
+	let passportFilePath = '/opt/gluu-server/etc/gluu/'+
+		'conf/passport-config.json'
+	if (fs.existsSync(passportFilePath)) {
+		return passportFilePath
+	} else {
+		let devenvPath = './devenv'
+		if(!fs.existsSync(devenvPath)) {
+			fs.mkdirSync(devenvPath)
+			fs.writeFileSync(
+				(devenvPath + '/passport-config.json'), JSON.stringify(passportConfigFileData)
+			)
+		}
+
+		return `${devenvPath}/passport-config.json`
+	}
+}
 
 module.exports = {
-	passportFile : '/opt/gluu-server/etc/gluu/conf/passport-config.json',
+	passportFile : passportConfigFilePath(),
 	saltFile : '/opt/gluu-server/etc/gluu/conf/salt',
 	timerInterval: 600000,
 }
