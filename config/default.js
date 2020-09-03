5c7f2275ee46666d66f97663de3a976dba4bb8c0
Christian <eland.christian@gmail.com>
2020-09-03 22:27:24 +0000

    fixed indent

--- config/default.js
+++ config/default.js
@@ -1,14 +1,16 @@
 const fs = require('fs')
 
 const passportConfigFileData = {
-	configurationEndpoint: "https://chris.gluuthree.org/identity/restv1/passport/config",
-	failureRedirectUrl: "https://chris.gluuthree.org/oxauth/auth/passport/passportlogin.htm",
-	logLevel: "debug",
+	configurationEndpoint:
+		'https://chris.gluuthree.org/identity/restv1/passport/config',
+	failureRedirectUrl:
+		'https://chris.gluuthree.org/oxauth/auth/passport/passportlogin.htm',
+	logLevel: 'debug',
 	consoleLogOnly: false,
-	clientId: "1502.3fe76d0a-38dd-4f91-830b-e33fd70d778a",
-	keyPath: "/tmp/passport-rp.pem",
-	keyId: "fbc267ef-0705-4b3a-8c80-bf70e75cf08b_sig_rs512",
-	keyAlg: "RS512"
+	clientId: '1502.3fe76d0a-38dd-4f91-830b-e33fd70d778a',
+	keyPath: '/tmp/passport-rp.pem',
+	keyId: 'fbc267ef-0705-4b3a-8c80-bf70e75cf08b_sig_rs512',
+	keyAlg: 'RS512'
 }
 
 /**
@@ -27,7 +29,8 @@ function passportConfigFilePath() {
 		if(!fs.existsSync(devenvPath)) {
 			fs.mkdirSync(devenvPath)
 			fs.writeFileSync(
-				(devenvPath + '/passport-config.json'), JSON.stringify(passportConfigFileData)
+				(devenvPath + '/passport-config.json'),
+				JSON.stringify(passportConfigFileData)
 			)
 		}
 
