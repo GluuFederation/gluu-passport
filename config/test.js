e4fdad63592045e9edae1c5f48563b2e15e54c2a
Christian <eland.christian@gmail.com>
2020-09-03 22:05:01 +0000

    config/test.js added passportConfig and root path concat

--- config/test.js
+++ config/test.js
@@ -1,29 +1,26 @@
 const saltFile = './test/testdata/salt'
-const timerInterval = 60000
+const timerInterval = 5000
 
-const passportFile = {
+const passportConfig = {
 	configurationEndpoint:
         'https://chris.gluuthree.org/identity/restv1/passport/config',
-
 	failureRedirectUrl:
         'https://chris.gluuthree.org/oxauth/auth/passport/passportlogin.htm',
-
 	logLevel: 'debug',
-
 	consoleLogOnly: false,
-
 	clientId: '1502.d49baf9f-b19b-40de-a990-33d08e7f9e77',
-
 	keyPath: './test/testdata/passport-rp.pem',
-
 	keyId: '36658e03-34ea-4745-ad43-959916c96def_sig_rs512',
-
 	keyAlg: 'RS512'
 }
 
+const root = process.cwd()
+const passportFile = `${root}/test/testdata/passport-config.json`
+// const passportFile = '/test/testdata/passport-config.json'
 
 module.exports = {
 	saltFile,
-	passportFile,
+	passportConfig,
 	timerInterval,
+	passportFile
 }
