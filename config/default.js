93dac1b0ae85b3ffd8d97d3cb73ecf861f8d09b9
Christian <eland.christian@gmail.com>
2020-09-07 21:37:11 +0000

    fixed defaultjs

--- config/default.js
+++ config/default.js
@@ -1,17 +1,6 @@
 const fs = require('fs')
 
-const passportConfigFileData = {
-	configurationEndpoint:
-		'https://chris.gluuthree.org/identity/restv1/passport/config',
-	failureRedirectUrl:
-		'https://chris.gluuthree.org/oxauth/auth/passport/passportlogin.htm',
-	logLevel: 'debug',
-	consoleLogOnly: false,
-	clientId: '1502.3fe76d0a-38dd-4f91-830b-e33fd70d778a',
-	keyPath: '/tmp/passport-rp.pem',
-	keyId: 'fbc267ef-0705-4b3a-8c80-bf70e75cf08b_sig_rs512',
-	keyAlg: 'RS512'
-}
+
 
 /**
  * Gets the default passport configuration file path.
@@ -19,27 +8,27 @@ const passportConfigFileData = {
  * @returns {string} passport-config.json file path
  */
 
-function passportConfigFilePath() {
-	let passportFilePath = '/opt/gluu-server/etc/gluu/'+
-		'conf/passport-config.json'
-	if (fs.existsSync(passportFilePath)) {
-		return passportFilePath
-	} else {
-		let devenvPath = './devenv'
-		if(!fs.existsSync(devenvPath)) {
-			fs.mkdirSync(devenvPath)
-			fs.writeFileSync(
-				(devenvPath + '/passport-config.json'),
-				JSON.stringify(passportConfigFileData)
-			)
-		}
-
-		return `${devenvPath}/passport-config.json`
-	}
-}
+// function passportConfigFilePath() {
+// 	let passportFilePath = '/opt/gluu-server/etc/gluu/'+
+// 		'conf/passport-config.json'
+// 	if (fs.existsSync(passportFilePath)) {
+// 		return passportFilePath
+// 	} else {
+// 		let devenvPath = './devenv'
+// 		if(!fs.existsSync(devenvPath)) {
+// 			fs.mkdirSync(devenvPath)
+// 			fs.writeFileSync(
+// 				(devenvPath + '/passport-config.json'),
+// 				JSON.stringify(passportConfigFileData)
+// 			)
+// 		}
+//
+// 		return `${devenvPath}/passport-config.json`
+// 	}
+// }
 
 module.exports = {
-	passportFile : passportConfigFilePath(),
+	passportFile : '/opt/gluu-server/etc/gluu/conf/passport-config.json',
 	saltFile : '/opt/gluu-server/etc/gluu/conf/salt',
 	timerInterval: 60000,
 }
