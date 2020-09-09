f799f5375d422bbfc6663486e08c4e1cb9399cf1
Chris Eland <eland.christian@gmail.com>
2020-09-09 12:49:59 +0000

    small fixes

--- config/default.js
+++ config/default.js
@@ -1,31 +1,3 @@
-const fs = require('fs')
-
-
-
-/**
- * Gets the default passport configuration file path.
- * If file doesn't exists, creates it and return path
- * @returns {string} passport-config.json file path
- */
-
-// function passportConfigFilePath() {
-// 	let passportFilePath = '/opt/gluu-server/etc/gluu/'+
-// 		'conf/passport-config.json'
-// 	if (fs.existsSync(passportFilePath)) {
-// 		return passportFilePath
-// 	} else {
-// 		let devenvPath = './devenv'
-// 		if(!fs.existsSync(devenvPath)) {
-// 			fs.mkdirSync(devenvPath)
-// 			fs.writeFileSync(
-// 				(devenvPath + '/passport-config.json'),
-// 				JSON.stringify(passportConfigFileData)
-// 			)
-// 		}
-//
-// 		return `${devenvPath}/passport-config.json`
-// 	}
-// }
 
 module.exports = {
 	passportFile : '/opt/gluu-server/etc/gluu/conf/passport-config.json',
