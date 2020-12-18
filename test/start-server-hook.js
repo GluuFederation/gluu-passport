5c68553d54611e11fee3caa9dff71c25f2dac0f0
Christian Eland <eland.christian@gmail.com>
2020-12-17 23:26:52 -0300

    test(hooks): remove exports
    As it should be loaded using `file` instead of `require` in mocha

--- test/start-server-hook.js
+++ test/start-server-hook.js
@@ -1,13 +1,11 @@
-exports.mochaHooks = {
-  before (done) {
-    /**
-     * Wait for server to start (event appStarted) to start tests
-     */
-    const server = require('../server/app')
-    server.on('appStarted', function () {
-      // remember you need --timeout on mocha CLI to be around 20000
-      console.log('app started event listened...')
-      done()
-    })
-  }
-}
+/**
+ * Wait for server to start (event appStarted) to start tests
+ */
+before((done) => {
+  const server = require('../server/app')
+  server.on('appStarted', function () {
+    // remember you need --timeout on mocha CLI to be around 20000
+    console.log('app started event listened...')
+    done()
+  })
+})
