79d220e6233188c9dc78b275607d60bf8a159424
christian-hawk <eland.christian@gmail.com>
2021-01-11 18:34:21 +0000

    test(enpoint-metric-steps): changed before hook to act like the other step files

--- test/features/steps/enpoint-metric-steps.js
+++ test/features/steps/enpoint-metric-steps.js
@@ -1,27 +1,40 @@
-const { Given, When, Then, BeforeAll } = require('cucumber')
-const got = require('got')
+const { Given, When, Then, Before, After } = require('cucumber')
 const chai = require('chai')
 const assert = chai.assert
-const helper = require('../../helper')
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
 
-BeforeAll({ timeout: 600 * 1000 }, (done) => {
-  helper.setupServer()
-    .then(() => done())
+let app
+let requester
+
+Before('@endpointMetrics', async () => {
+  app = require('../../../server/app')
+  await app.on('appStarted', () => {
+    console.log('app started...')
+  })
+  requester = chai.request(app).keepOpen()
+  await app.rateLimiter.resetKey('::ffff:127.0.0.1')
 })
 
+After('endpointMetrics', async () => {
+  requester.close()
+})
 Given('passport server is up and running', async () => {
-  const response = await got('http://127.0.0.1:8090/passport/health-check')
+  const response = await requester.get('/passport/health-check')
+  // const response = await got('http://127.0.0.1:8090/passport/health-check')
   assert.equal(response.statusCode, 200,
     'response.statusCode is NOT 200')
 })
 
 When('my aggregator access metrics endpoint', async () => {
-  const response = await got('http://127.0.0.1:8090/passport/metrics')
+  const response = await requester.get('/passport/metrics')
+  // const response = await got('http://127.0.0.1:8090/passport/metrics')
   assert.isNotNull(response)
 })
 
 Then('should return me the metrics', async () => {
-  const response = await got('http://127.0.0.1:8090/passport/metrics')
+  const response = await requester.get('http://127.0.0.1:8090/passport/metrics')
+  // const response = await got('http://127.0.0.1:8090/passport/metrics')
   assert.propertyVal(response.headers, 'content-type', 'text/plain')
 })
 
@@ -31,7 +44,8 @@ Given(
     let counter = 0
     while (counter < numberOfTimes) {
       try {
-        await got(`http://127.0.0.1:8090${endpoint}`, { throwHttpErrors: false })
+        await requester.get(`${endpoint}`)
+        // await got(`http://127.0.0.1:8090${endpoint}`, { throwHttpErrors: false })
       } catch (err) {
         // empty block for errors status code (i.e. 404)
       }
@@ -43,8 +57,10 @@ Given(
 Then(
   '{string} count should be {int}',
   async function (endpointAlias, numberOfTimes) {
-    const response = await got('http://127.0.0.1:8090/passport/metrics')
-    const body = response.body
+    const response = await requester.get('/passport/metrics')
+    // const response = await got('http://127.0.0.1:8090/passport/metrics')
+    // console.log(response)
+    const body = response.text
     const pattern = new RegExp(
       `http_request_duration_seconds_count{status_code="\\d+",method="GET",path="${endpointAlias}.*`
     )
