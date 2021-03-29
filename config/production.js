d5d9547951fae07ad8392e19688617dcbc07bc5d
christian-hawk <eland.christian@gmail.com>
2021-03-29 15:40:03 +0000

    ensure sameSite value is none in production

--- config/production.js
+++ config/production.js
@@ -4,5 +4,5 @@ module.exports = {
   timerInterval: 60000,
   rateLimitWindowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
   rateLimitMaxRequestAllow: 1000,
-  sameSite: ''
+  sameSite: 'none'
 }
