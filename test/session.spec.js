639386fd2bead6728aa773e9c080bd6d4f5b9637
christian-hawk <eland.christian@gmail.com>
2021-03-30 17:26:38 +0000

    test(session): ensure cookies settings are loaded

---
+++ test/session.spec.js
@@ -0,0 +1,37 @@
+/* eslint-disable security/detect-non-literal-fs-filename */
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const config = require('config')
+
+chai.use(chaiHttp)
+
+// it is indirectly used at here res.should.have.status
+// eslint-disable-next-line no-unused-vars
+const should = chai.Should()
+
+/**
+ * Integration test using localhost (not mocked)
+ */
+describe('/passport/metrics - metrics endpoint (integration)', () => {
+  const gluuBasePath = 'http://127.0.0.1:8090'
+
+  // server should be up and running, integration test
+  // health check used to inspect req.session.cookie
+  it('Health check - GET /passport/health-check', (done) => {
+    const sameSiteConfig = config.get('sameSite')
+    const secureConfig = config.get('secure')
+    chai.request(gluuBasePath)
+      .get('/passport/health-check')
+      .set('Cookie', 'SameSite=None')
+      .end((_err, res) => {
+        console.log(res.body)
+        res.body.sessionCookie.should.include(
+          {
+            sameSite: sameSiteConfig,
+            secure: secureConfig
+          }
+        )
+        done()
+      })
+  })
+})
