559f37ccfb49c21723ae3ebd431928ca8dfdf638
kdhttps <kdhttps@gmail.com>
2021-03-30 23:17:42 +0530

    feat(session.js session.test.js): make separate file for session config
    feat #242

--- server/app-factory.js
+++ server/app-factory.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express()
-const session = require('express-session')
-const MemoryStore = require('memorystore')(session)
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const passport = require('passport')
@@ -9,10 +7,10 @@ const morgan = require('morgan')
 const logger = require('./utils/logging')
 const routes = require('./routes')
 const metricsMiddleware = require('../server/utils/metrics')
-const { randomSecret } = require('./utils/misc')
 const { globalErrorHandler } = require('./utils/error-handler')
 const flash = require('connect-flash')
 const { rateLimiter } = require('./utils/rate-limiter')
+const { session } = require('./utils/session')
 
 class AppFactory {
   createApp () {
@@ -24,19 +22,7 @@ class AppFactory {
     app.use(flash())
     app.use(rateLimiter)
     app.set('trust proxy', 1)
-    app.use(session({
-      cookie: {
-        maxAge: 86400000,
-        sameSite: 'none',
-        secure: true
-      },
-      store: new MemoryStore({
-        checkPeriod: 86400000 // prune expired entries every 24h
-      }),
-      secret: randomSecret(),
-      resave: false,
-      saveUninitialized: false
-    }))
+    app.use(session)
 
     app.use(passport.initialize())
     app.use(passport.session())
--- test/app-factory.test.js
+++ test/app-factory.test.js
@@ -105,3 +105,26 @@ describe('rateLimiter middleware', () => {
     sinon.restore()
   })
 })
+
+describe('session middleware', () => {
+  const rewiredSession = appFactoryRewire.__get__('session')
+  const { session } = require('../server/utils/session')
+
+  it('should exist', () => {
+    assert.exists(rewiredSession)
+  })
+
+  it('should be a function', () => {
+    assert.isFunction(rewiredSession)
+  })
+
+  it('should be equal session middleware', () => {
+    assert.equal(rewiredSession, session)
+  })
+
+  it('should be called once as app.use arg', () => {
+    const appUseSpy = spyOnAppUse()
+    assertCalledWithFunctionAsArg(appUseSpy, rewiredSession)
+    sinon.restore()
+  })
+})
