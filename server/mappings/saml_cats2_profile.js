6a28044c1280026802d9894aa88a945e516a3b7c
Doug Harris <doug.harris@xsilosystems.com>
2021-05-06 10:02:38 -0400

    feat(saml_cats2_profile.js): capture new SAML attributes

--- server/mappings/saml_cats2_profile.js
+++ server/mappings/saml_cats2_profile.js
@@ -1,6 +1,9 @@
 module.exports = profile => {
     return {
             uid: profile.nameID,
+            issuer: profile.issuer,
             persistentId: profile.spNameQualifier+'|'+profile.nameQualifier+'|'+profile.nameID,
+            sessionIndex: profile.sessionIndex,
+            authnInstant: profile.getAssertion().Assertion.AuthnStatement[0].$.AuthnInstant
     }
 }
