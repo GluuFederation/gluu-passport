8ecca3c3e51a89a1ff53cc4324acdc69c101fb94
Doug Harris <doug.harris@xsilosystems.com>
2021-05-27 15:53:55 -0400

    Reduce timer interval
    oxTrust needs more time to start

--- config/production.js
+++ config/production.js
@@ -1,7 +1,7 @@
 module.exports = {
   passportFile: '/etc/gluu/conf/passport-config.json',
   saltFile: '/etc/gluu/conf/salt',
-  timerInterval: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
+  timerInterval: 2 * 60 * 1000, // 2 minutes in milliseconds
   rateLimitWindowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
   rateLimitMaxRequestAllow: 100000,
   appInsightsKey: process.env.APPINSIGHTS_INSTRUMENTATIONKEY,
