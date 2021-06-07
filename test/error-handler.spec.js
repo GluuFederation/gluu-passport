7b5e97e4bbc8c28e7628ec2797d2e4a9ce0341bb
kdhttps <kdhttps@gmail.com>
2021-06-07 14:55:59 +0530

    test(error-handler.spec.js): check error with correct message
    test #250

--- test/error-handler.spec.js
+++ test/error-handler.spec.js
@@ -2,33 +2,26 @@
 const chai = require('chai')
 const sinon = require('sinon')
 const got = require('got')
+const config = require('config')
 const { globalErrorHandler } = require('../server/utils/error-handler.js')
 
 const assert = chai.assert
+const passportConfig = config.get('passportConfig')
 
 async function generateStrategyError () {
-  const provider = 'apple'
-  const options = {
-    method: 'POST',
-    url: `http://127.0.0.1:8090/passport/auth/${provider}/callback`,
-    json: {
-      state: 'xxxxxxxxxxxx',
-      error: 'user_cancelled_authorize'
-    },
-    responseType: 'json',
-    throwHttpErrors: false,
-    followRedirect: true
-  }
-  const response = await got(options)
+  const response = await got(
+    'http://127.0.0.1:8090/passport/error',
+    { throwHttpErrors: false, followRedirect: false }
+  )
   const headers = response.headers
-  assert.equal(headers.location, '/passport/error')
+  assert.equal(headers.location, `${passportConfig.failureRedirectUrl}?failure=An%20error%20occurred`)
 }
 
 describe('error-handler.js test', () => {
-  it('should call globalErrorHandler with correct error message', () => {
+  it('should call globalErrorHandler with correct error message', async () => {
     const globalErrorHandlerSpy = sinon.spy(globalErrorHandler)
-    generateStrategyError()
-    assert(globalErrorHandlerSpy.calledOnce)
+    await generateStrategyError()
+    sinon.assert.calledOnce(globalErrorHandlerSpy)
     sinon.restore()
   })
 })
