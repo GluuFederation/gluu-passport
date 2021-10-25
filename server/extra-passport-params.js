722e7fcd36880d9260f4bf07d833fbb04ca52a9b
Mus Radi <radi_mus2005@yahoo.fr>
2021-10-25 12:10:57 -0400

    Implementation of a clean way of SAML dynamic options passing

--- server/extra-passport-params.js
+++ server/extra-passport-params.js
@@ -18,7 +18,7 @@ const params = R.once(() => [
       decryptionCert: fs.readFileSync(global.config.spTLSCert, 'utf-8'),
       privateKey: fs.readFileSync(process.env.PRIVATE_KEY || '/etc/certs/passport-sp.key', 'utf-8'),
       getSamlOptions: function(request, done) {
-        return done(null, request.query);
+        return done(null, (request.locals != null ? request.locals.dynamic_options : {}));
       }
     }
   },
