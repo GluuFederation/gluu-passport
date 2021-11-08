949ca1d7fde6e95fde639ad5b18888ae8905023c
kdhttps <kdhttps@gmail.com>
2021-11-08 12:00:05 +0530

    feat(production.js): add envs to config values

--- config/production.js
+++ config/production.js
@@ -1,11 +1,19 @@
+let cookieSecure = process.env.PASSPORT_COOKIE_SECURE
+
+if (cookieSecure === undefined) {
+  cookieSecure = true
+} else {
+  cookieSecure = cookieSecure === 'true'
+}
+
 module.exports = {
   passportFile: '/etc/gluu/conf/passport-config.json',
   saltFile: '/etc/gluu/conf/salt',
-  timerInterval: 60000,
-  rateLimitWindowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
-  rateLimitMaxRequestAllow: 1000,
-  cookieSameSite: 'none',
-  cookieSecure: true,
+  timerInterval: parseInt(process.env.PASSPORT_TIMER_INTERVAL) || 60000,
+  rateLimitWindowMs: parseInt(process.env.PASSPORT_RATE_LIMIT_WINDOW_MS) || 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
+  rateLimitMaxRequestAllow: parseInt(process.env.PASSPORT_RATE_LIMIT_MAX_REQUEST_ALLOW) || 1000,
+  cookieSameSite: process.env.PASSPORT_COOKIE_SAME_SITE || 'none',
+  cookieSecure,
   HTTP_PROXY: process.env.HTTP_PROXY,
   HTTPS_PROXY: process.env.HTTPS_PROXY,
   NO_PROXY: process.env.NO_PROXY
