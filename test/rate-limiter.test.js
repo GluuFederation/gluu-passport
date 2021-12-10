e6c1f3b2997124dfd196f4dabe3a98c3918b5a9b
kdhttps <kdhttps@gmail.com>
2021-12-10 18:00:00 +0530

    test(rate-limiter.test.js): test rate limiter

--- test/rate-limiter.test.js
+++ test/rate-limiter.test.js
@@ -1,49 +1,24 @@
-
 const chai = require('chai')
-const assert = chai.assert
 const rewire = require('rewire')
-const rateLimiterRewire = rewire('../server/utils/rate-limiter')
-const config = require('config')
-const rateLimit = require('express-rate-limit')
+const rateLimiter = rewire('../server/utils/rate-limiter')
+const assert = chai.assert
 
 describe('rate-limiter.js test', () => {
-  const windowMs = rateLimiterRewire.__get__('windowMs')
-  const max = rateLimiterRewire.__get__('max')
-  const rateLimiter = rateLimiterRewire.__get__('rateLimiter')
-
-  it('windowMs should exist', () => {
-    assert.exists(windowMs)
+  const configure = rateLimiter.__get__('configure')
+  it('configure should be exist', () => {
+    assert.exists(configure)
   })
 
-  it('windowMs should be number', () => {
-    assert.isNumber(windowMs)
+  it('configure should be function', () => {
+    assert.isFunction(configure)
   })
 
-  it('windowMs should be equals to config value', () => {
-    assert.equal(windowMs, config.get('rateLimitWindowMs'))
+  const rateLimit = rateLimiter.__get__('rateLimit')
+  it('rateLimit should be exist', () => {
+    assert.exists(rateLimit)
   })
 
-  it('max should exist', () => {
-    assert.exists(max)
-  })
-
-  it('max should be number', () => {
-    assert.isNumber(max)
-  })
-
-  it('max should be equals to config value', () => {
-    assert.equal(max, config.get('rateLimitMaxRequestAllow'))
-  })
-
-  it('rateLimiter should exist', () => {
-    assert.exists(rateLimiter)
-  })
-
-  it('rateLimiter should be function', () => {
-    assert.isFunction(rateLimiter)
-  })
-
-  it('rateLimiter should be equals to express-rate-limit module', () => {
-    assert.isFunction(rateLimiter, rateLimit())
+  it('rateLimit should be function', () => {
+    assert.isFunction(rateLimit)
   })
 })
