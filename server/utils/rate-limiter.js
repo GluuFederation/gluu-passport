f33c83e31872fe98e746e8faf7f4d187775cb556
kdhttps <kdhttps@gmail.com>
2021-12-10 18:03:23 +0530

    refactor(rate-limiter.js): remove env config

--- server/utils/rate-limiter.js
+++ server/utils/rate-limiter.js
@@ -1,16 +1,16 @@
 const rateLimit = require('express-rate-limit')
-const config = require('config')
+const logger = require('./logging')
 
-const windowMs = config.get('rateLimitWindowMs')
-const max = config.get('rateLimitMaxRequestAllow')
-
-const rateLimiter = rateLimit({
-  windowMs,
-  max,
-  message: `You have exceeded the ${max} requests in ${windowMs} milliseconds limit!`,
-  headers: true
-})
+const configure = (windowMs = 20000, max = 1000) => {
+  logger.log2('info', `configure rate-limiter windowMs=${windowMs} maxRequestAllow=${max}`)
+  return rateLimit({
+    windowMs,
+    max,
+    message: `You have exceeded the ${max} requests in ${windowMs} milliseconds limit!`,
+    headers: true
+  })
+}
 
 module.exports = {
-  rateLimiter
+  configure
 }
