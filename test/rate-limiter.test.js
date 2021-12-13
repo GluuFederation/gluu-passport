89e559f692bc67f1dc501ab998d011d8ec08274b
kdhttps <kdhttps@gmail.com>
2021-12-13 14:53:42 +0530

    test(rate-limiter.test.js): test rate limit reconfig

--- test/rate-limiter.test.js
+++ test/rate-limiter.test.js
@@ -1,5 +1,8 @@
 const chai = require('chai')
 const rewire = require('rewire')
+const sinon = require('sinon')
+
+const logger = require('../server/utils/logging')
 const rateLimiter = rewire('../server/utils/rate-limiter')
 const assert = chai.assert
 
@@ -11,6 +14,27 @@ describe('rate-limiter.js test', () => {
 
   it('configure should be function', () => {
     assert.isFunction(configure)
+  })
+
+  it('configure should init a express-rate-limit with resetKey function', () => {
+    const app = {}
+    configure(app, 10000, 10)
+    assert.exists(app.rateLimiter)
+    assert.isDefined(app.rateLimiter.resetKey)
+  })
+
+  it('configure should configure with correct values', () => {
+    const checkWindowMs = rateLimiter.__get__('checkWindowMs')
+    const checkMax = rateLimiter.__get__('checkMax')
+    assert.equal(checkWindowMs, 10000)
+    assert.equal(checkMax, 10)
+  })
+
+  it('configure should not reconfigure express-rate-limit once values are same', () => {
+    const sinonSpy = sinon.spy(logger, 'log2')
+    configure({}, 10000, 10)
+    assert.isTrue(sinonSpy.calledOnceWith('debug', 'Skip ratelimit config, already config with same values'))
+    sinonSpy.restore()
   })
 
   const rateLimit = rateLimiter.__get__('rateLimit')
