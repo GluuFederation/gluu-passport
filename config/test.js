4ffebd4478da5cf368c1437bb0b9cb9effb56471
kdhttps <kdhttps@gmail.com>
2021-12-15 13:33:52 +0530

    test(test.js): add session config

--- config/test.js
+++ config/test.js
@@ -43,6 +43,10 @@ const passportConfigAuthorizedResponse = {
     rateLimit: {
       windowMs: 24 * 60 * 60 * 1000,
       max: 100
+    },
+    session: {
+      cookieSameSite: 'none',
+      cookieSecure: true
     }
   },
   idpInitiated: {
