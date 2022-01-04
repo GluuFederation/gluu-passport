0a43b08cd7dc1bdfdbf3f87e327a040f1c241edd
kdhttps <kdhttps@gmail.com>
2022-01-04 14:26:54 +0530

    test(openid-client-helper.spec.js): refactor test

--- test/openid-client-helper.spec.js
+++ test/openid-client-helper.spec.js
@@ -10,6 +10,7 @@ describe('Integration Test OpenID Client Helper', () => {
   const testProvider = passportConfigAuthorizedResponse.providers.find(p => p.id === 'oidccedev6privatejwt')
   let kid = null
   const jwksFilePath = `../server/jwks/${testProvider.id}.json`
+  let jwks
 
   describe('generateJWKS test', () => {
     const generateJWKS = rewiredOpenIDClientHelper.__get__('generateJWKS')
@@ -19,15 +20,24 @@ describe('Integration Test OpenID Client Helper', () => {
       assert.exists(jwksFilePath, `${jwksFilePath} file not found`)
     })
 
-    it('make sure jwks has keys, kid, n and kty', () => {
-      const jwks = require(jwksFilePath)
+    it('jwks should have keys', () => {
+      jwks = require(jwksFilePath)
       assert.isArray(jwks.keys, 'keys not found in jwks')
+    })
+
+    it('jwks should have kid', () => {
       kid = jwks.keys[0].kid
-      const n = jwks.keys[0].n
-      const kty = jwks.keys[0].kty
       assert.exists(kid, 'kid not found in jwks')
-      assert.exists(kty, 'kty not found in jwks')
+    })
+
+    it('jwks should have n', () => {
+      const n = jwks.keys[0].n
       assert.exists(n, 'n not found in jwks')
+    })
+
+    it('jwks should have kty', () => {
+      const kty = jwks.keys[0].kty
+      assert.exists(kty, 'kty not found in jwks')
     })
 
     it('make sure generateJWKS not regenerating jwks again and rewrite existing jwks data', async () => {
