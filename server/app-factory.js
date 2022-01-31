2287ac82c4db6868f9fbf4061a44fc66c8bb15c8
kdhttps <kdhttps@gmail.com>
2022-01-31 17:10:43 +0530

    refactor(app-factory.js): add modules using esm import

--- server/app-factory.js
+++ server/app-factory.js
@@ -1,18 +1,18 @@
-const express = require('express')
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import passport from 'passport'
+import morgan from 'morgan'
+import logger from './utils/logging'
+import routes from './routes'
+import metricsMiddleware from '../server/utils/metrics'
+import { globalErrorHandler } from './utils/error-handler'
+import flash from 'connect-flash'
+import { rateLimiter } from './utils/rate-limiter'
+import { session } from './utils/session'
+import './utils/http-global-proxy'
 const app = express()
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const passport = require('passport')
-const morgan = require('morgan')
-const logger = require('./utils/logging')
-const routes = require('./routes')
-const metricsMiddleware = require('../server/utils/metrics')
-const { globalErrorHandler } = require('./utils/error-handler')
-const flash = require('connect-flash')
-const { rateLimiter } = require('./utils/rate-limiter')
-const { session } = require('./utils/session')
 // Setup http proxy config
-require('./utils/http-global-proxy')
 
 class AppFactory {
   createApp () {
@@ -43,4 +43,4 @@ class AppFactory {
   }
 }
 
-module.exports = AppFactory
+export { AppFactory, app, flash }
