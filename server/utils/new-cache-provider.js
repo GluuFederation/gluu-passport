3ae875088b4dc7c2fe6c0d9e3748d98a3d739264
christian <eland.christian@gmail.com>
2022-10-18 13:31:48 -0300

    feat(cacheProvider): return objects are functions

--- server/utils/new-cache-provider.js
+++ server/utils/new-cache-provider.js
@@ -1,8 +1,8 @@
 function get (type, options, expiration) {
   return {
-    saveAsync: '',
-    getAsync: '',
-    removeAsync: ''
+    saveAsync: () => {},
+    getAsync: () => {},
+    removeAsync: () => {}
   }
 }
 
--- test/new-cache-provider.test.js
+++ test/new-cache-provider.test.js
@@ -3,6 +3,7 @@ const cacheProvider = require('../server/utils/new-cache-provider')
 
 describe('cache-provider', () => {
   describe('get', () => {
+    // should implement new passport-saml object with async functions
     it('should have saveAsync', () => {
       const provider = cacheProvider.get('redis', {}, 60)
       assert.exists(provider.saveAsync)
@@ -14,6 +15,24 @@ describe('cache-provider', () => {
     it('should have removeAsync', () => {
       const provider = cacheProvider.get('redis', {}, 60)
       assert.exists(provider.removeAsync)
+    })
+    describe('saveAsync', () => {
+      it('should be a function', () => {
+        const provider = cacheProvider.get('redis', {}, 60)
+        assert.typeOf(provider.saveAsync, 'function')
+      })
+    })
+    describe('getAsync', () => {
+      it('should be a function', () => {
+        const provider = cacheProvider.get('redis', {}, 60)
+        assert.typeOf(provider.getAsync, 'function')
+      })
+    })
+    describe('removeAsync', () => {
+      it('should be a function', () => {
+        const provider = cacheProvider.get('redis', {}, 60)
+        assert.typeOf(provider.removeAsync, 'function')
+      })
     })
   })
 })
